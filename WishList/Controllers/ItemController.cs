d1fc977c17d5d79304cdacc42f1f1154b25e84e3
Madhumitha-spec <123074668+Madhumitha-spec@users.noreply.github.com>
2023-01-24 08:54:32 +0000

    Update ItemController.cs

--- WishList/Controllers/ItemController.cs
+++ WishList/Controllers/ItemController.cs
@@ -56,7 +56,7 @@ namespace WishList.Controllers
             var user = _userManager.GetUserAsync(HttpContext.User).Result;
             
                 var item = _context.Items.FirstOrDefault(e => e.Id == id);
-                if(item.user!=user) {
+                if(item.User!=user) {
                 return Unauthorized();
                 }
                 else{
@@ -64,11 +64,8 @@ namespace WishList.Controllers
                 _context.SaveChanges();
                 return RedirectToAction("Index");
                 }
-            }
-            else
-            {
-                return View("Error");
-            }
+            
+           
 
             
             
